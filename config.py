2aab67187fcb81048ed06203fc3b1c12bf59bc0f
flyscoot <82533166+flyscoot@users.noreply.github.com>
2021-04-15 00:32:08 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,68 @@
+'''
+flyscoot juniors Script
+My Principal Anyone Can Enjoy Premium Feature Without Buy It ^.^
+
+Any Suggest PM Me On Whatsapp or Telegram
+'''
+
+
+'''
+Fill Your flyscoot Account
+'''
+account = {
+	"Username":"juniors",
+	"Password":"alfharidzie"
+}
+'''
+Change Your Bet Strategy/Bet Setting
+'''
+betset = {
+	"Basebet":"0.001",
+	"Chance1":"40",
+	"Chance2":"55"
+}
+
+'''
+Select Bet Setting Tools
+Note :
+IFLose = Increase Bet After Lose
+IFWin = Increase Bet After Win
+'''
+tools = {
+	"IFLose":"2",
+	"IFWin":"1",
+	"TargetProfit":"0.01",
+	"TargetLose":"99999999999",
+	"ResetBetIfWin":"1",
+	"ResetBetIfLose":"0",
+	"ResetSeedEachBet":"ON"
+}
+
+'''
+Select Premium Feature ^.^
+Note :
+Interval In Second, Comma Allowed
+RefreshIfWin and RefreshIfLose Use Number to Use
+Example 
+RefreshIfWin : 3. Automaticaly Refresh After 3 Win Streak 0 to OFF
+RefreshIfLose : 3. Automaticaly Refresh After 3 Lose Streak 0 to OFF
+'''
+feature = {
+	"BetInterval":"0",
+	"BetIntervalLose":"0",
+	"BetInvervalWin":"0",
+	"IntervalRefresh":"0",
+	"RefreshIfWin":"0",
+	"RefreshIfLose":"0",
+	"AutoRefresh":"ON"
+}
+
+'''
+Dont Forget To Change Your Wallet Here
+'''
+withdraw = {
+	"AutoWithdraw":"OFF",
+	"AmountWithdraw":"5",
+	"WithdrawIfBalance":"5",
+	"Address":"xxx"
+}
